21e3d2487bea2445ec6b39e6f26992eb266fa2a3
msabo1 <mariosabo2@gmail.com>
2019-06-17 13:25:25 +0200

    Create generic IRepository and UserRepository

---
+++ AHP.Repository.Common/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHP.Repository.Common
+{
+    public interface IRepository<T>
+    {
+        Task<T> AddAsync(T entity);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIDAsync(Guid id);
+        Task<T> UpdateAsync(T oldEntity, T newEntity);
+        Task<int> DeleteAsync(T entity);
+    }
+}
