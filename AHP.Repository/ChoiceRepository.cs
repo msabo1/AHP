c5dc43c65e63d56371fbce0df65d12105f8c1f7b
Lovro Buday <budaylovro5@gmail.com>
2019-06-18 11:09:38 +0200

    ChoiceRepository change

--- AHP.Repository/ChoiceRepository.cs
+++ AHP.Repository/ChoiceRepository.cs
@@ -5,60 +5,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AHP.DAL;
+using AHP.Model;
 using AHP.Repository.Common;
+using AutoMapper;
 
 namespace AHP.Repository
 {
-    class ChoiceRepository : IRepository<Choice>
+    class ChoiceRepository
     {
 
         private AHPEntities _context;
+        IMapper _mapper;
 
-        public ChoiceRepository(AHPEntities context)
+        public ChoiceRepository(AHPEntities context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public async Task<Choice> AddAsync(Choice choice)
+        public async Task<ChoiceModel> AddAsync(ChoiceModel choice)
         {
-            _context.Choices.Add(choice);
+            _context.Choices.Add(_mapper.Map<ChoiceModel, Choice>(choice));
             await _context.SaveChangesAsync();
             return choice;
         }
 
-        public async Task<List<Choice>> AddRangeAsync(List<Choice> choices)
+        public async Task<List<ChoiceModel>> AddRangeAsync(List<ChoiceModel> choices)
         {
-            _context.Choices.AddRange(choices);
+            _context.Choices.AddRange(_mapper.Map<List<ChoiceModel>, List<Choice>>(choices));
             await _context.SaveChangesAsync();
             return choices;
         }
 
-        public async Task<int> DeleteAsync(Choice choice)
+        public async Task<int> DeleteAsync(ChoiceModel choice)
         {
-            _context.Choices.Remove(choice);
+            _context.Choices.Remove(_mapper.Map<ChoiceModel, Choice>(choice));
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<List<Choice>> GetAllAsync()
+        public async Task<List<ChoiceModel>> GetAllAsync()
         {
             var choices = await _context.Choices.ToListAsync();
-            return choices;
+            return _mapper.Map<List<Choice>, List<ChoiceModel>>(choices);
         }
 
-        public async Task<Choice> GetByIDAsync(Guid id)
+        public async Task<ChoiceModel> GetByIDAsync(Guid id)
         {
             var choice = await _context.Choices.Where(c => c.ChoiceID == id).FirstAsync();
             await _context.Entry(choice).Collection(c => c.Criteria).LoadAsync();
             await _context.Entry(choice).Collection(a => a.Alternatives).LoadAsync();
-            return choice;
+            return _mapper.Map<Choice, ChoiceModel>(choice);
         }
 
-        public async Task<Choice> UpdateAsync(Choice oldChoice, Choice newChoice)
+        public async Task<ChoiceModel> UpdateAsync(ChoiceModel oldChoice, ChoiceModel newChoice)
         {
-            var choice = await _context.Choices.Where(c => c == oldChoice).FirstAsync();
+            var _oldChoice = _mapper.Map<ChoiceModel, Choice>(oldChoice);
+            var choice = await _context.Choices.Where(c => c == _oldChoice).FirstAsync();
             _context.Entry(choice).CurrentValues.SetValues(newChoice);
             await _context.SaveChangesAsync();
-            return newChoice;
+            return _mapper.Map<Choice, ChoiceModel>(choice);
         }
     }
 }
