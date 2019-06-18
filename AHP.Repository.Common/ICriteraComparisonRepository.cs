f774304f6594f071af0892f5d3abc07626e6cc71
msabo1 <mariosabo2@gmail.com>
2019-06-18 12:15:33 +0200

    Create ICriteriaComparisonRepository and CriteriaComparisonRepository

---
+++ AHP.Repository.Common/ICriteraComparisonRepository.cs
@@ -0,0 +1,14 @@
+﻿using AHP.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHP.Repository.Common
+{
+    public interface ICriteraComparisonRepository : IRepository<CriteriaComparisonModel>
+    {
+        Task<CriteriaComparisonModel> GetByIDsAsync(Guid id1, Guid id2);
+    }
+}
