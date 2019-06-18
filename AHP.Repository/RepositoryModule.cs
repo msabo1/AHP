f115c7da94c20cc4ee8f202d96adfc8df1a860d4
msabo1 <mariosabo2@gmail.com>
2019-06-18 11:26:01 +0200

    Add RepositoryModule

--- AHP.Repository/RepositoryModule.cs
+++ AHP.Repository/RepositoryModule.cs
@@ -1,18 +1,21 @@
-﻿using AHP.Repository.Common;
+﻿using AHP.DAL;
+using AHP.Repository.Common;
 using Autofac;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AHP.Repository
 {
-    class RepositoryModule : Module
+    class RepositoryModule : Autofac.Module
     {   //Register components in Load()
         protected override void Load(ContainerBuilder builder)
         {
-            
+            builder.RegisterType<AHPEntities>().AsSelf();
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(Repository))).Where(t => t.Name.EndsWith("Repository"));
 
         }
     }
