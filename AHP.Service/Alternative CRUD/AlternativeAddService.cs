5c9279bd6495078adffd2697a85a3863afea9bc7
Vlatko Klen <Vlaja_Klen@hotmail.com>
2019-06-24 11:20:22 +0200

    Add alternative controller

--- AHP.Service/Alternative CRUD/AlternativeAddService.cs
+++ AHP.Service/Alternative CRUD/AlternativeAddService.cs
@@ -1,28 +1,24 @@
 ﻿using AHP.Model.Common;
 using AHP.Repository.Common;
-using AHP.Service.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AHP.Service
+namespace AHP.Service.Alternative_CRUD
 {
-    class UserDeleteService : IUserDeleteService
+    class AlternativeAddService
     {
         IUnitOfWork _unitOfWork;
 
-        public UserDeleteService(IUnitOfWork unitOfWork)
+        public AlternativeAddService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
-
-        public bool Delete(IUserModel user)
+         public void Add(IAlternativeModel alternativa)
         {
-            return _unitOfWork.UserRepository.Delete(user);
-        }
 
+        }
     }
 }
-
---
+++ AHP.WebAPI/Controllers/AlternativeController.cs
@@ -0,0 +1,50 @@
+﻿using AHP.Model.Common;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AHP.WebAPI.Controllers
+{
+    public class AlternativeController : ApiController
+    {
+        IAlternativeAddService _alternativeAdd;
+        IMapper _mapper;
+        public AlternativeController(
+            IAlternativeAddService alternativeAdd;
+            IMapper mapper
+            )
+        {
+            _alternativeAdd = alternativeAdd;
+            _mapper = mapper;
+           
+        }
+
+
+        [Route("Alternative/Add")]
+        public async Task<IHttpActionResult> Post(AlternativeControllerModel alternative)
+        {
+            var _alternative = _mapper.Map<AlternativeControllerModel, IAlternativeModel>(alternative);
+            var status = await _alternativeAdd.Add(_alternative);
+
+            if (status != null)
+                return Ok(_mapper.Map<IAlternativeModel, AlternativeControllerModel>(status));
+            else
+                return NotFound();
+        }
+
+    }
+    public class AlternativeControllerModel
+    {
+        public ICollection<IAlternativeComparisonModel> AlternativeComparisons1 { get; set; }
+        public System.Guid AlternativeID { get; set; }
+        public string AlternativeName { get; set; }
+        public Nullable<double> AlternativeScore { get; set; }
+        public System.Guid ChoiceID { get; set; }
+    }
+}
+
