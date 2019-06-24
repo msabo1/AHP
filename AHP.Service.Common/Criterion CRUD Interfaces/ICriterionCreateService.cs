354fc6882e63d832cf9b946e732397532491ce74
matej1231 <cvarak8@gmail.com>
2019-06-24 14:03:27 +0200

    Criterion CRUD

---
+++ AHP.Service.Common/Criterion CRUD Interfaces/ICriterionCreateService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using AHP.Model.Common;
+
+namespace AHP.Service.Common
+{
+    public interface ICriterionUpdateService
+    {
+        Task<ICriterionUpdateService> Check(ICriterionUpdateService criterion);
+    }
+}
